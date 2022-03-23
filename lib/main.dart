2c6e0f6443f8f967de255e7f66de4fdf7fc44abc
thinhhja2001 <19522281@gm.uit.edu.vn>
2022-03-23 22:00:15 +0700

    configure firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:learning_english_app/firebase_options.dart';
 import 'package:learning_english_app/providers/signin_provider.dart';
 import 'package:learning_english_app/providers/signup_provider.dart';
 import 'package:learning_english_app/screens/signup_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
